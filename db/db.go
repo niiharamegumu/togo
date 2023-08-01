14209770f65049632a8ceabb6dc389b407cf05ff
Niihara Megumu <nhrmgm1223@gmail.com>
2023-08-01 19:23:41 +0900

    add get root path

--- db/db.go
+++ db/db.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/niiharamegumu/togo/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -11,13 +14,21 @@ var (
 	err error
 )
 
-// ConnectDB データベースに接続する関数
 func ConnectDB() (*gorm.DB, error) {
-	if DB == nil {
-		DB, err = gorm.Open(sqlite.Open("tasks.db"), &gorm.Config{})
-		if err != nil {
-			return nil, err
-		}
+	if DB != nil {
+		return DB, nil
+	}
+
+	rootPath :=  os.Getenv("TOGO_PROJECT_ROOT_PATH")
+	if rootPath == "" {
+		fmt.Println(".envに TOGO_PROJECT_ROOT_PATH を設定してください。")
+		return nil, fmt.Errorf("環境変数 TOGO_PROJECT_ROOT_PATH が設定されていません")
+	}
+
+	dbPath := fmt.Sprintf("%s/%s", os.Getenv("TOGO_PROJECT_ROOT_PATH"), "tasks.db")
+	DB, err = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		return nil, err
 	}
 
 	DB.AutoMigrate(&models.Task{})

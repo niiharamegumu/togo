1dd830a89eb397b9adc6dcaf1c98114683c95cd8
Niihara Megumu <nhrmgm1223@gmail.com>
2023-08-02 22:13:15 +0900

    Change the update command to be interactive

--- cmd/updateCmd.go
+++ cmd/updateCmd.go
@@ -1,17 +1,21 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/niiharamegumu/togo/models"
 	"github.com/spf13/cobra"
 )
 
 var updateCmd = &cobra.Command{
-	Use:     "update [id] [title]",
+	Use:     "update [id]",
 	Short:   "Update a task's title",
 	Aliases: []string{"u"},
-	Example: "togo update [id] [\"Update title\"]",
+	Example: "togo update [id]",
 	Run:     updateTask,
 }
 
@@ -20,13 +24,16 @@ func init() {
 }
 
 func updateTask(cmd *cobra.Command, args []string) {
-	if len(args) < 2 {
-		fmt.Println("❌ Please specify the task ID and title")
+	if len(args) < 1 {
+		fmt.Println("❌ Please specify the task ID")
 		return
 	}
 
-	taskID := args[0]
-	taskTitle := args[1]
+	taskID, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("❌ Invalid task ID. Please provide a valid integer ID.")
+		return
+	}
 
 	var task models.Task
 	result := dbConn.First(&task, taskID)
@@ -35,13 +42,30 @@ func updateTask(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	task.Title = taskTitle
+	task.RenderTaskTable()
+
+	fmt.Printf("Enter the new task title : ")
+	reader := bufio.NewReader(os.Stdin)
+	newTitle, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("🚨 Error reading input:", err)
+		return
+	}
+
+	newTitle = strings.TrimSpace(newTitle)
+
+	if newTitle == "" {
+		fmt.Println("👌 No changes made")
+		return
+	}
+
+	task.Title = newTitle
 	result = dbConn.Save(&task)
 	if result.Error != nil {
 		fmt.Println("🚨 Failed to update the task:", result.Error)
 		return
 	}
 
-	fmt.Printf("👉 Update Task ID %v \n", task.ID)
+	fmt.Printf("👉 Updated Task ID %v \n", task.ID)
 	task.RenderTaskTable()
 }
